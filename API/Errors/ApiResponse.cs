e906366c8360d25742210c03bc3174a07cae1062
bvigga@gmail.com <bvigga@gmail.com>
2023-03-05 21:56:55 +1100

    End of section 5

---
+++ API/Errors/ApiResponse.cs
@@ -0,0 +1,28 @@
+namespace API.Errors
+{
+  public class ApiResponse
+    {
+       
+        public ApiResponse(int statusCode, string message = null)
+        {
+            Message = message?? GetDefaultMessageForStatusCode(statusCode);
+            StatusCode = statusCode;
+
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; } 
+
+        private string GetDefaultMessageForStatusCode(int statusCode)
+        {
+            return statusCode switch 
+            {
+                400 => "A bad request, you have made",
+                401 => "Authorized, you are not",
+                404 => "Resource found, it was not",
+                500 => "Errors are the path to the dark side. Errors lead to anger. Anger leds to hate. Hate leads to career change",
+                _ => null
+            };
+        }
+    }
+}
